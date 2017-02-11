220cd8b9563698aad90fe8e0b7d1a881ee4422d2
hieunguyen8794 <hieu.nguyen.8794@gmail.com>
2017-02-11 20:13:39 +0700

    bo sung

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "hello, world! chapter3"
+    render html: "hello, world! chapter3 nhe moi nguoi"
   end
 end
