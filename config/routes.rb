1becdfa9c1b7cb6321f1a88a9f1b8843ed739cc2
hieunguyen8794 <hieu.nguyen.8794@gmail.com>
2017-02-21 13:57:39 +0700

    chapter14

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,10 @@ Rails.application.routes.draw do
   get    '/login',   to: 'session#new'
   post   '/login',   to: 'session#create'
   delete '/logout',  to: 'session#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships,       only: [:create, :destroy]
 end
